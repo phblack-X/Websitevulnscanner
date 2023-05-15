7df3c059454c06d976823b8c84bfea4a76454aa8
Tite <133258888+phblack-X@users.noreply.github.com>
2023-05-15 18:19:16 +0800

    Update Websitevulnscanner.py

--- Websitevulnscanner.py
+++ Websitevulnscanner.py
@@ -1,29 +1,18 @@
 import requests
-
 import os
-
 os.system('clear')
-
-print("""\033[92m[+]pogi ako[+]
-
-╔═══════════════════ ══
-
-║Vulnerability scanner║
-
-╚═══════════════════ ══
-
+print("""\033[92m
+╔═════════════════════╗
+║VULNERABILITY SCANNER║
+╚═════════════════════╝
 \033[91m[+]CODED BY PH.BL4CK[+]
-
 """)
-
 domain = str(input("[+]ENTER THE TARGET SITE :\033[91m"))
 
 headers = requests.get(domain).headers
 
 if 'x-frame-Options' in headers:
-
         print(" \033[91m [NOT VULNERABLE] " + domain)
 
 else:
-
     print("\033[92m[VULNERABLE] " + domain) 
